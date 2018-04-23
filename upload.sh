92d031d79a2d6f97148e31093132ec1eb393e5d4
LiuXiaoxiaoo <928972678@qq.com>
2018-04-23 13:29:11 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@ function getdir(){
 	 if [ -f $1 ]
 	    then
 	        #/usr/local/php/bin/php /www/wwwroot/***/one.php upload:file $1 /share/$1>> upload.log
-		/usr/bin/php /www/wwwroot/***/one.php upload:file $1 /share/$1>> upload.log
+		/usr/bin/php /www/wwwroot/***/one.php upload:file $1 $1>> upload.log
 	        rm -rf $1
 	        echo '删除上传完成'$1 >> upload.log
         	exit 0
@@ -19,7 +19,7 @@ function getdir(){
             echo $i
             echo $dir_or_file
             #/usr/local/php/bin/php /www/wwwroot/***/one.php upload:file $dir_or_file /share/$dir_or_file >> upload.log
-	    /usr/bin/php /www/wwwroot/***/one.php upload:file $dir_or_file /share/$dir_or_file >> upload.log
+	    /usr/bin/php /www/wwwroot/***/one.php upload:file $dir_or_file $dir_or_file >> upload.log
 	    rm -rf $dir_or_file
 	    echo '删除完成.'$dir_or_file >> upload.log
 	    if [ "$i" == "$length" ]
