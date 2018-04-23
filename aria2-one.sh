e78e89d3948cad31000efb8e3a07ae337598c0c8
LiuXiaoxiaoo <928972678@qq.com>
2018-04-23 12:21:39 +0800

    Create aria2-one.sh

---
+++ aria2-one.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SRC=$3
+
+downloadpath='/网站地址/down/'
+cd $downloadpath
+if [ "$2" == "0" ]; then
+	echo $1'没有文件'$SRC>> upload.log
+  exit 0
+else
+
+#删掉些BT中没用的辣鸡文件
+find "$SRC" -regextype posix-extended -regex ".*\.(chm|torrent|htm|html|url|nfo|jpg|bmp|png|ico|gif)" -exec rm -f {} \;
+
+path=${SRC##*$downloadpath}
+echo $1'准备上传'$path>> upload.log
+cd $downloadpath
+bash upload.sh $path
+exit $3
+fi
