4385e32b1bd02b6a320d61cf869b39e7edea454e
LiuXiaoxiaoo <928972678@qq.com>
2018-12-21 12:14:56 +0800

    Update aria2-one.sh

--- aria2-one.sh
+++ aria2-one.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-
-SRC=$3
-
-downloadpath='/root/Download/'
-cd $downloadpath
-if [ "$2" == "0" ]; then
-	echo $1'没有文件'$SRC>> upload.log
-  exit 0
-else
-
-#删掉些BT中没用的辣鸡文件
-find "$SRC" -regextype posix-extended -regex ".*\.(chm|torrent|htm|html|url|nfo|jpg|bmp|png|ico|gif)" -exec rm -f {} \;
-
-path=${SRC##*$downloadpath}
-echo $1'准备上传'$path>> upload.log
-cd $downloadpath
-bash /root/upload.sh $path
-exit $3
+path=$3
+downloadpath='/root/Download'
+if [ $2 -eq 0 ]
+        then
+                exit 0
 fi
+while true; do  #提取下载文件根路径，如把/root/downloads/a/b/c/d.jpg变成/root/Download
+filepath=$path
+path=${path%/*};
+if [ "$path" = "$downloadpath" ] && [ $2 -eq 1 ]  #如果下载的是单个文件
+    then
+    /usr/bin/php /www/wwwroot/one.hsru.tk/one.php upload:file "$filepath" /upload/ >> /root/upload.log
+    rm -rf "$filepath"
+    echo '删除完成'"$filepath" >> upload.log
+    exit 0
+elif [ "$path" = "$downloadpath" ]   #文件夹
+    then
+    /usr/bin/php /www/wwwroot/one.hsru.tk/one.php  upload:folder "$filepath"/ /upload/"${filepath##*/}"/ >> /root/upload.log
+    rm -rf "$filepath"/ 
+    echo '删除完成'"$filepath"/ >> upload.log
+    exit 0
+fi
+done
